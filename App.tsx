5668038eb9e4986a071d5ebdb186cf35eecfeb0f
Furkan Berk Yılmazer <furkanberkyilmazer@users.noreply.github.com>
2024-04-30 00:03:30 +0300

    react native word learn app

--- App.tsx
+++ App.tsx
@@ -1,117 +1,118 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import words from './words.json';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const [repeatFlag, setRepeatFlag] = useState(true);
+  const [wordList, setWordList] = useState([...words]);
+  const [gosterFlag, setGosterFlag] = useState(true);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const handleKnown = () => {
+    const updatedWords = [...wordList];
+    updatedWords[index].known = true;
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    setIndex(index + 1);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    const lastIndex = [...wordList].reverse().findIndex(word => !word.known);
+    const lastFalseIndex = lastIndex !== -1 ? wordList.length - 1 - lastIndex : -1;
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    if (index >= lastFalseIndex) {
+      setIndex(0);
+      const updatedUnknownWords = wordList.filter(word => !word.known);
+      setWordList(updatedUnknownWords);
+      if (wordList.filter(word => !word.known).length <= 0) {
+        setRepeatFlag(false);
+      }
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+  const handleUnknown = () => {
+    setIndex(index + 1);
+    setRepeatFlag(true);
+
+    const lastIndex = [...wordList].reverse().findIndex(word => !word.known);
+    const lastFalseIndex = lastIndex !== -1 ? wordList.length - 1 - lastIndex : -1;
+
+    if (index >= lastFalseIndex) {
+      setIndex(0);
+      const updatedUnknownWords = wordList.filter(word => !word.known);
+      setWordList(updatedUnknownWords);
+    }
+  };
+
+  const handleGoster = () => {
+    setGosterFlag(!gosterFlag);
+  };
+  
+  const handleReset = () => {
+    const updatedWordsData = words.map(word => ({
+      ...word,
+      known: false
+    }));
+    setWordList(updatedWordsData);
+    setIndex(0);
+    setRepeatFlag(true);
+  };
+
+
+  if (!repeatFlag) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Sık Kullanılan Kelimeler</Text>
+        <Text style={styles.message}>Tebrikler! Tüm kelimeleri öğrendiniz.</Text>
+        <Button title="Tekrar Et" onPress={handleReset} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Sık Kullanılan Kelimeler</Text>
+        {gosterFlag ? (
+          <View>
+            <Text style={styles.word}>{index + 1}. Kelime: {wordList[index].word}</Text>
+            <Text style={styles.translation}>Türkçe: {wordList[index].translation}</Text>
+            <Button title="Biliyorum" onPress={handleKnown} />
+            <Button title="Bilmiyorum" onPress={handleUnknown} />
+            <Button title="Gösterme" onPress={handleGoster} />
+          </View>
+        ) : (
+          <View>
+            <Text style={styles.word}>{index + 1}. Kelime: {wordList[index].word}</Text>
+            <Button title="Biliyorum" onPress={handleKnown} />
+            <Button title="Bilmiyorum" onPress={handleUnknown} />
+            <Button title="Göster" onPress={handleGoster} />
+          </View>
+        )}
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
+  header: {
     fontSize: 24,
-    fontWeight: '600',
+    marginBottom: 20,
+    color: '#535353',
   },
-  sectionDescription: {
-    marginTop: 8,
+  message: {
     fontSize: 18,
-    fontWeight: '400',
+    color: '#666',
   },
-  highlight: {
-    fontWeight: '700',
+  word: {
+    fontSize: 20,
+    marginBottom: 10,
+    color: '#666',
+  },
+  translation: {
+    marginBottom: 20,
+    color: '#666',
   },
 });
 
